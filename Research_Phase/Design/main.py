283ca80f27d480f046848d9cec1443a29713e7d8
Sali Mallat <s.mallat01@gmail.com>
2022-09-09 17:29:23 +0100

    update

---
+++ Research_Phase/Design/main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+header = st.beta_container()
+dataset = st.beta_container()
+features = st.beta_constainer()
+
+with header:
+    st.Title('Welcome to my first StreamLit App')
+    
